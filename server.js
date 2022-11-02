e9b108024c08705840ea232a0d4ba27865354e0a
Azhar Raza <walisystems123@gmail.com>
2022-11-02 17:30:14 +0500

    initial commit at

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server,{
+    cors: {origin:"*"}
+});
+
+io.on('connection',(socket)=>{
+    console.log('connection');
+    //socket on mean receiving something
+    socket.on('sendChatToServer',(message)=>{
+        console.log(message);
+    });
+
+    //socket emit mean sending something
+    // socket.emit();
+
+    socket.on('disconnect',(socket)=>{
+        console.log('Disconnect');
+    });
+});
+
+server.listen(8000,()=>{
+    console.log('server is running');
+});
